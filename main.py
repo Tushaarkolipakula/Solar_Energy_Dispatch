2e8fb7c1b1ffb81cab0cd2dd7322d0e5fde621ee
Tejas Kollipara <tejas.hs.k999@gmail.com>
2025-11-21 12:00:57 +0530

    Columns Extraction from CSV File

--- main.py
+++ main.py
@@ -23,3 +23,33 @@ GRID_ARBITRAGE_PENALTY = 0.05
 
 DT = 1.0
 SOLVER_LIMIT = 300
+
+# TOU pricing (override)
+OVERRIDE_PRICES = True
+BUY_OFFPEAK = 5
+BUY_PEAK = 12.0
+PEAK_HOURS = list(range(17, 22))
+SELL_FIXED = 3.0
+
+# Load the CSV file
+df = pd.read_csv(CSV_FILE)
+
+# Removing Extra Spaces
+df.columns = [col.strip() for col in df.columns]
+
+# Converting into lowercase
+col_map = {col.lower(): col for col in df.columns}
+
+load_col = col_map["demand"]
+pv_col = col_map["solar_gen"]
+buy_col = col_map["grid_buy_price"]
+sell_col = col_map["grid_sell_price"]
+
+# Timestamp
+timestamp_col = "Timestamp"
+df[timestamp_col] = pd.to_datetime(df[timestamp_col])
+
+# Converting into float and filling empty values to 0.0
+Pload_raw = pd.to_numeric(df[load_col], errors="coerce").fillna(0.0).values
+Ppv_raw = pd.to_numeric(df[pv_col], errors="coerce").fillna(0.0).values
+
